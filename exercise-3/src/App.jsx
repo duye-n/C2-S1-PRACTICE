76d4a5d294a95d8b9dfb01d75af81814547d309d
duyen <duyen.dinh25@student.passerellesnumeriques.org>
2024-05-25 10:32:48 +0700

    exercise 3

--- exercise-3/src/App.jsx
+++ exercise-3/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { ALL_PNV_TEACHERS } from "./teachers.js";
 
 // NO change to perform here...
+  export default App;
 export function User({ user }) {
   return (
     <div id="user" data-testid="user">
@@ -12,15 +13,26 @@ export function User({ user }) {
     </div>
   );
 }
-
+export function Teacher({ teacher }) {
+  return (
+    <div id="user" data-testid="user">
+      <h2>{teacher.firstName}</h2>
+      <p>{teacher.lastName}</p>
+      <p>{teacher.title}</p>
+    </div>
+  );
+}
 function App() {
   return (
     <div id="app">
       <h1>PNV Team !!</h1>
       <p>Here are some PNV trainers and educators, do you know them?</p>
-      <User /> {/* How to loop on ALL_PNV_TEACHERS list ? */}
+
+      {ALL_PNV_TEACHERS.map((teacher, index) => (
+        <Teacher key={index} teacher={teacher} />
+      ))}
     </div>
   );
 }
 
-export default App;
+
